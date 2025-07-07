145542c7ad726a43e68e9442c2a38cd84d548c7a
Nico <nherresk@gmail.com>
2025-07-07 10:43:56 -0500

    Excuse generator in JS

---
+++ script.js
@@ -0,0 +1,18 @@
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function generateExcuse() {
+let whoRandom = who[Math.floor(Math.random() * who.length)];
+let actionRandom = action[Math.floor(Math.random() * action.length)];
+let whatRandom = what[Math.floor(Math.random() * what.length)];
+let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+let excuse = whoRandom + ' ' + actionRandom + ' ' + whatRandom + ' ' + whenRandom;
+return excuse;
+}
+
+window.onload = function () {
+    document.getElementById("excuse").innerHTML = generateExcuse();
+};
