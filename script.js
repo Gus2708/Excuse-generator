c396947a9c6cd570f13e1beb3974423b28160be9
gusdev <59888502+Gus2708@users.noreply.github.com>
2025-12-18 02:01:22 +0000

    Optimized and organized code: - Fixed indentation issues in JavaScript - Used template literal for the `excuse` variable to interpolate multiple expressions - Improved HTML styles with clearer spacing using Bootstrap

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
 function generateExcuse() {
-let whoRandom = who[Math.floor(Math.random() * who.length)];
-let actionRandom = action[Math.floor(Math.random() * action.length)];
-let whatRandom = what[Math.floor(Math.random() * what.length)];
-let whenRandom = when[Math.floor(Math.random() * when.length)];
+    let whoRandom = who[Math.floor(Math.random() * who.length)];
+    let actionRandom = action[Math.floor(Math.random() * action.length)];
+    let whatRandom = what[Math.floor(Math.random() * what.length)];
+    let whenRandom = when[Math.floor(Math.random() * when.length)];
 
-let excuse = whoRandom + ' ' + actionRandom + ' ' + whatRandom + ' ' + whenRandom;
-return excuse;
-}
+    let excuse = `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`;
+
+    return excuse;
+};
 
 window.onload = function () {
     document.getElementById("excuse").innerHTML = generateExcuse();
